146dac4ddb6c77ac7b25c485ba069fbbb53b7352
Chad Wilson <robotnewyork@yahoo.com>
2023-07-30 20:36:32 -0400

    Adding data items

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -5,7 +5,13 @@
 <div class="about">
 	<h2>About C3.SYS</h2>
 
-	<p>About us...</p>
+	<p>C3.SYS is a record label and production studio based in Detroit, Michigan. Founded in 2022 by Chad Wilson, C3.SYS creates and distributes music from independent artists.</p>
+
+	<p>C3.SYS is the spiritual successor to C2.SYS, which was started by Jeff Goergen, Chad Wilson and Jesse Barnes in the late 1990s, and focused mainly on experimental electronic and metal music.</p>
+		
+	<p>This site makes the C2.SYS and C3.SYS catalogs available as much as possible.</p>
+		
+	
 </div>
 
 <style>
